38e92c845166fb4e80f43111a7f80ce143d1eb86
Maroof <ss19co049.maroof.siddiqui@gmail.com>
2023-07-16 14:32:19 +0530

    Added message listener - Receive & Disconnect

--- js/client.js
+++ js/client.js
@@ -1,11 +1,41 @@
 const socket = io('http://127.0.0.1:8000');           
 // note don't write localhost:8000 there a difference between 127.0.0.1 & localhost in socket.io
-const form = document.getElementById('send-container');
+const form = document.getElementById('sent-container');
 
-const message = document.getElementById('messageInp');
+const messageInput = document.getElementById('messageInput');
 const messageContainer = document.querySelector('.container');
 
+const append =  (message,position) =>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
 
 const name_of_user = prompt('Please enter your name:');
 socket.emit('new-user-joined', name_of_user);
-console.log('user joined at client side',name_of_user);
+
+//display the name of the user at client side.
+// console.log('user joined at client side',name_of_user);
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value = '';
+})
+
+socket.on('user-joined', name =>{
+    append(`${name} joined the chat`,'left');
+})
+
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`,'left');
+})
+
+socket.on('left', data =>{
+    append(`${data.name} left the chat`,'left');
+})
+
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -18,12 +18,17 @@ const users = {};
 
 io.on('connection', socket =>{
     socket.on('new-user-joined', name =>{
+        console.log('inside io.on with ', name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined',name);
     });
-    console.log('inside io.on');
 
     socket.on('send', message =>{
-        socket.broadcast.emit('recieve', {message:message, name: users[socket.id] })
+        socket.broadcast.emit('receive', {message:message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left',{name : users[socket.id]});
+        delete users[socket.id];
     });
 })
